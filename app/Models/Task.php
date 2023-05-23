cca3ef6b86a69f8bf14f943d54b472bdc6114957
TAISEI0704 <132648502+TAISEI0704@users.noreply.github.com>
2023-05-23 20:33:18 +0800

    モデル,マイグレーションファイル作成

---
+++ app/Models/Task.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    protected $fillable = [
+        'title',
+        'contents',
+        'image_at',
+        'user_id',
+        'date',
+    ];
+
+    use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo("App\Models\User");
+    }
+
+    public function bookmarks()
+    {
+        return $this->hasMany('App\Models\Bookmark');
+    }
+    
+    public function comments()
+    {
+        return $this->hasMany('App\Models\Comment');
+    }
+}
--- database/migrations/2023_05_23_094039_create_comments_table.php
+++ database/migrations/2023_05_23_094039_create_comments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateCommentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name', 30);
-            $table->string('email', 30)->unique();
-            $table->string('password', 90);
+            $table->text('body');
+            $table->unsignedBigInteger('task_id');
+            $table->foreign('task_id')->references('id')->on('tasks');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
@@ -29,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('comments');
     }
 }
