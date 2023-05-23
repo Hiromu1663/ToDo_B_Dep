f1f6ef0a3330dc96f8a142b9c4c44a9a78792a87
Hiromu1663 <132648643+Hiromu1663@users.noreply.github.com>
2023-05-24 00:12:35 +0800

    簡易show.php完成

---
+++ resources/views/show.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="{{ asset("css/show.css") }}">
+  <title>タスク一覧</title>
+</head>
+<body>
+  <div class="function">
+    <p>todoリスト期限順に並び替え</p>
+  </div>
+ 
+  {{-- <div class="chunks">
+  @foreach($tasks->chunk(3) as $chunk)
+    <div class="chunk">
+      @foreach($chunk as $task)
+      <div class="task">
+        <div class="title">{{ $task->title }}</div>
+        <div class="content">{{ $task->contents }}</div>
+      </div>
+      @endforeach
+    </div>
+  @endforeach
+  </div> --}}
+
+  <div class="chunks">
+    <div class="chunk">
+      <div class="task">
+        <div class="title">title</div>
+        <div class="content">content</div>
+      </div>
+      <div class="task">
+        <div class="title">title</div>
+        <div class="content">content</div>
+      </div>
+      <div class="task">
+        <div class="title">title</div>
+        <div class="content">content</div>
+      </div>
+      <div class="task">
+        <div class="title">title</div>
+        <div class="content">content</div>
+      </div>
+    </div>
+
+    <div class="chunk">
+      <div class="task">
+        <div class="title">title</div>
+        <div class="content">content</div>
+      </div>
+      <div class="task">
+        <div class="title">title</div>
+        <div class="content">content</div>
+      </div>
+      <div class="task">
+        <div class="title">title</div>
+        <div class="content">content</div>
+      </div>
+      <div class="task">
+        <div class="title">title</div>
+        <div class="content">content</div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
