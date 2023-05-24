8dba6c95cea315f9aa8b9b8ddd16c9a11c167530
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-05-24 15:21:27 +0800

    editのお問い合わせフォーム大枠完成

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -4,41 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset("css/edit.css") }}">
+    <title>編集</title>
 </head>
-<body>
-    <div class="header-left">
-        <img class="logo" src="./logo.png" alt="">
-    </div>
-    <div  class="header-right">
-        <ul class="nav">
-            <li><a href="#">name</a></li>
-        </ul>
-    </div>
-    </header>
-    <div class="container">
-    <div class="row justify-content-center">
-    <div class="col-md-8">
-        <form  method="POST">
-            @csrf
-            <div class="form-group">
-                <label>日付</label>
-                <input type="date" class="form-control" value="">
-            </div>
-            <div class="form-group">
-                <label>共有先</label>
-                <input type="text" class="form-control" value="" name="title">
-            </div>
-            <div class="form-group">
-                <label>タスク内容</label>
-                <textarea class="form-control" rows="5" name="body"></textarea>
-            </div>
-    
-            <button type="submit" class="btn btn-primary">登録する</button>
-        </form>
-    </div>
-    </div>
-    </div>
-
-</body>
+    <table class="form-table">
+        <tbody>
+            <tr>
+                <th>タイトル</th>
+                <td><input type="text" size="60" value="">
+                </td>
+            </tr>
+            <tr>
+                <th>日付</th>
+                <td><input type="date" size="60" value="">
+                </td>
+            </tr>
+            </tr>
+            <tr>
+                <th>詳細</th>
+                <td><textarea name="content" id="" cols="30" rows="10" placeholder="お問い合わせ内容"></textarea>
+                </td>
+            </tr>
+            <button type="Submit">送信</button>
+        </tbody>
+    </table>
 </html>
