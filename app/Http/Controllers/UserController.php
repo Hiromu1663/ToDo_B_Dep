fd656c61a248dfa35a0100aa95a9392695ddf112
Megumi0310 <132648399+Megumi0310@users.noreply.github.com>
2023-05-24 14:07:06 +0800

    UserController修正/BookmarkController作成

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    //
+    public function showProfile($id)
+    {
+        $user = User::find($id);
+        
+        return view('profile', ['user'=>$user]);
+        
+    }
 }
