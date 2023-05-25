a52dc5e0a9b6b851a759c303cdc28ed166d165f8
Megumi0310 <132648399+Megumi0310@users.noreply.github.com>
2023-05-25 14:54:55 +0800

    migration:userにavatorを追記

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name', 30);
             $table->string('email', 30)->unique();
             $table->string('password', 90);
+            $table->string('avatar');
             $table->timestamps();
         });
     }
