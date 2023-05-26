71c888dc78dc933f95c6c17567249fd6f978b62b
TAISEI0704 <132648502+TAISEI0704@users.noreply.github.com>
2023-05-26 10:56:20 +0800

    コメントをindexページに反映

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -49,6 +49,25 @@
         @else
         <a href="/tasks/{{ $task->id }}/bookmarks">ブックマーク</a> 
         @endif
+        {{-- コメント機能 --}}
+        <div>
+          <a href="{{ route('comments.create',$task->id) }}">コメントする</a>
+        </div>
+        {{-- <div class="row justify-content-center"> --}}
+        <div>
+          <div class="">
+            コメント一覧
+            @foreach ($task->comments as $comment)
+              <div class="card mt-3">
+                  {{-- <h5 class="card-header">投稿者：{{ $comment->user->name }}</h5> --}}
+                  <div class="card-body">
+                      {{-- <h5 class="card-title">投稿日時：{{ $comment->created_at }}</h5> --}}
+                      <p class="card-text">内容：{{ $comment->body }}</p>
+                  </div>
+              </div>            
+            @endforeach
+          </div>
+        </div>
       </div>
       @endforeach
     </div>
