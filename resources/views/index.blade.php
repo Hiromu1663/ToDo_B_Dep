87ff694507cdf0d1bcec33eda2f772f86f73185f
Hiromu1663 <132648643+Hiromu1663@users.noreply.github.com>
2023-05-29 01:01:36 +0800

    show.css→index.cssへ名称変更。css内容微修正。
    .task{
    margin: 0px 15px;
    ↓
    margin: 0 0.5%;
    }

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="{{ asset("css/app.css") }}">
-  <link rel="stylesheet" href="{{ asset("css/show.css") }}">
+  <link rel="stylesheet" href="{{ asset("css/index.css") }}">
   {{-- <script src="{{ asset("js/app.js") }}"></script> --}}
   <title>タスク一覧</title>
 </head>
