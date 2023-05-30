17a13937ae78f13df02dfffb28d2b32034f6d339
TAISEI0704 <132648502+TAISEI0704@users.noreply.github.com>
2023-05-30 13:00:04 +0800

    タスクの写真表示変更、詳細ボタン追加

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -35,60 +35,65 @@
     <div class="chunk">
       @foreach($chunk as $task)
       <div class="task">
+        @if($task->image_at !== null)
         <div class="image_at">
           <img src="{{ asset('storage/images/'.$task->image_at) }}" alt="">
         </div>
+        @endif
         <div class="title">{{ $task->title }}</div>
         <div class="content">{{ $task->contents }}</div>
-        <div>
-          {{-- 編集機能追加 --}}
-          @if($task->user_id == Auth::user()->id)
-           <form action="{{ route('tasks.edit', [$task->id]) }}" method="POST">
-            @csrf
-            @method('get')
-            <input type="submit" value="編集">
-           </form>
-          @endif
-        </div>
-        <div> 
-          {{-- 削除機能追加 --}}
+        <div class="detail-btn"><button>詳細</button></div>
+        <div class="box">
+          <div>
+            {{-- 編集機能追加 --}}
             @if($task->user_id == Auth::user()->id)
-              <form action="{{ route('tasks.destroy', [$task->id]) }}" method="POST">
-                @csrf
-                @method('delete')
-              <input type="submit" value="削除">
-              </form>
+            <form action="{{ route('tasks.edit', [$task->id]) }}" method="POST">
+              @csrf
+              @method('get')
+              <input type="submit" value="編集">
+            </form>
             @endif
-        </div>
-        @if($task->bookmarkedBy(Auth::user())->exists())
-        <a href="/bookmarks/{{ $task->bookmarkedBy(Auth::user())->firstOrfail()->id }}">ブックマークを外す</a>
-        @else
-        <a href="/tasks/{{ $task->id }}/bookmarks">ブックマーク</a> 
-        @endif
-        {{-- コメント機能 --}}
-        <div>
-          <a href="{{ route('comments.create',$task->id) }}">コメントする</a>
-        </div>
-        {{-- <div class="row justify-content-center"> --}}
-        <div>
-          <div class="">
-            コメント一覧
-            @foreach ($task->comments as $comment)
-              <div class="card mt-3">
-                  {{-- <h5 class="card-header">投稿者：{{ $comment->user->name }}</h5> --}}
-                  <div class="card-body">
-                      {{-- <h5 class="card-title">投稿日時：{{ $comment->created_at }}</h5> --}}
-                      <p class="card-text">内容：{{ $comment->body }}</p>
-                      @if($comment->user_id == Auth::user()->id)
-                      <form action="{{ route('comments.destroy', [$comment->id]) }}" method="POST">
-                        @csrf
-                        @method('delete')
-                      <input type="submit" value="削除">
-                      </form>
-                    @endif
-                  </div>
-              </div>            
-            @endforeach
+          </div>
+          <div> 
+            {{-- 削除機能追加 --}}
+              @if($task->user_id == Auth::user()->id)
+                <form action="{{ route('tasks.destroy', [$task->id]) }}" method="POST">
+                  @csrf
+                  @method('delete')
+                <input type="submit" value="削除">
+                </form>
+              @endif
+          </div>
+          @if($task->bookmarkedBy(Auth::user())->exists())
+          <a href="/bookmarks/{{ $task->bookmarkedBy(Auth::user())->firstOrfail()->id }}">ブックマークを外す</a>
+          @else
+          <a href="/tasks/{{ $task->id }}/bookmarks">ブックマーク</a> 
+          @endif
+          {{-- コメント機能 --}}
+          <div>
+            <a href="{{ route('comments.create',$task->id) }}">コメントする</a>
+          </div>
+          {{-- <div class="row justify-content-center"> --}}
+          <div>
+            <div class="">
+              コメント一覧
+              @foreach ($task->comments as $comment)
+                <div class="card mt-3">
+                    {{-- <h5 class="card-header">投稿者：{{ $comment->user->name }}</h5> --}}
+                    <div class="card-body">
+                        {{-- <h5 class="card-title">投稿日時：{{ $comment->created_at }}</h5> --}}
+                        <p class="card-text">内容：{{ $comment->body }}</p>
+                        @if($comment->user_id == Auth::user()->id)
+                        <form action="{{ route('comments.destroy', [$comment->id]) }}" method="POST">
+                          @csrf
+                          @method('delete')
+                        <input type="submit" value="削除">
+                        </form>
+                      @endif
+                    </div>
+                </div>            
+              @endforeach
+            </div>
           </div>
         </div>
       </div>
