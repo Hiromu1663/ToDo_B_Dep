4c760e3692c7e05695cb343a76c2dde938e524c4
TAISEI0704 <132648502+TAISEI0704@users.noreply.github.com>
2023-05-31 14:16:55 +0800

    index, bookmark, profileページ編集

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -60,34 +60,41 @@
         @endif
         <div class="title">{{ $task->title }}</div>
         <div class="content">{{ $task->contents }}</div>
-        {{-- <div class="sub"> --}}
+
           <div class="detail-btn">詳細</div>
           <div class="box">
-            <div>
-              {{-- 編集機能追加 --}}
-              @if($task->user_id == Auth::user()->id)
-              <form action="{{ route('tasks.edit', [$task->id]) }}" method="POST">
-                @csrf
-                @method('get')
-                <input type="submit" value="編集">
-              </form>
+            <div class="date">投稿日：{{ $task->created_at->format('Y-m-d') }}</div>
+            <div class="limit">
+              @php
+                  $now = \Carbon\Carbon::now();
+                  $dueDate = \Carbon\Carbon::parse($task->date);
+                  $daysRemaining = $dueDate->diffInDays($now);
+              @endphp
+
+              @if ($daysRemaining <= 3)
+                <span class="limit-text">期限：あと{{ $daysRemaining }}日</span>
+              @else
+                期限：あと{{ $daysRemaining }}日
               @endif
             </div>
-            <div> 
-              {{-- 削除機能追加 --}}
+            <div class="priority">{{ $task->priority }}</div>
+            <div class="btn-container">
+                {{-- 編集機能追加 --}}
                 @if($task->user_id == Auth::user()->id)
-                  <form action="{{ route('tasks.destroy', [$task->id]) }}" method="POST">
-                    @csrf
-                    @method('delete')
-                  <input type="submit" value="削除">
-                  </form>
+                <form action="{{ route('tasks.edit', [$task->id]) }}" method="POST">
+                  @csrf
+                  @method('get')
+                  <input type="submit" value="編集">
+                </form>
+                {{-- 削除機能追加 --}}
+                <form action="{{ route('tasks.destroy', [$task->id]) }}" method="POST">
+                  @csrf
+                  @method('delete')
+                <input type="submit" value="削除">
+                </form>
                 @endif
             </div>
-            @if($task->bookmarkedBy(Auth::user())->exists())
-            <a href="/bookmarks/{{ $task->bookmarkedBy(Auth::user())->firstOrfail()->id }}">ブックマークを外す</a>
-            @else
-            <a href="/tasks/{{ $task->id }}/bookmarks">ブックマーク</a> 
-            @endif
+
             {{-- コメント機能 --}}
             <div>
               <a href="{{ route('comments.create',$task->id) }}">コメントする</a>
@@ -115,6 +122,13 @@
               </div>
             </div>
           </div>
+          <div class="bookmark">
+            @if($task->bookmarkedBy(Auth::user())->exists())
+            <a href="/bookmarks/{{ $task->bookmarkedBy(Auth::user())->firstOrfail()->id }}">ブックマークを外す</a>
+            @else
+            <a href="/tasks/{{ $task->id }}/bookmarks">ブックマーク</a> 
+            @endif
+          </div>
         {{-- </div> --}}
       </div>
       @endforeach
@@ -122,44 +136,4 @@
   @endforeach
   </div>
   {{ $tasks->links() }}
-
-  {{-- <div class="chunks">
-    <div class="chunk">
-      <div class="task">
-        <div class="title">title</div>
-        <div class="content">content</div>
-      </div>
-      <div class="task">
-        <div class="title">title</div>
-        <div class="content">content</div>
-      </div>
-      <div class="task">
-        <div class="title">title</div>
-        <div class="content">content</div>
-      </div>
-      <div class="task">
-        <div class="title">title</div>
-        <div class="content">content</div>
-      </div>
-    </div>
-
-    <div class="chunk">
-      <div class="task">
-        <div class="title">title</div>
-        <div class="content">content</div>
-      </div>
-      <div class="task">
-        <div class="title">title</div>
-        <div class="content">content</div>
-      </div>
-      <div class="task">
-        <div class="title">title</div>
-        <div class="content">content</div>
-      </div>
-      <div class="task">
-        <div class="title">title</div>
-        <div class="content">content</div>
-      </div>
-    </div>
-  </div> --}}
   @endsection
