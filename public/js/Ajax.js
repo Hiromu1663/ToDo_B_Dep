a52e29d156300a77aca2fd4a4a1d550d37e29a11
Hiromu1663 <132648643+Hiromu1663@users.noreply.github.com>
2023-06-02 12:44:16 +0800

    Ajax改良。

--- public/js/Ajax.js
+++ public/js/Ajax.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
     let bookmarkUrl = bookmarkLink.attr('href'); 
     let csrfToken = $('meta[name="csrf-token"]').attr('content');
     let isBookmarked = bookmarkLink.has('i.fas.fa-bookmark').length > 0;
-    // let targetBookmark = bookmarkLink.closest('.bookmark');
+    let targetBookmark = bookmarkLink.closest('.bookmark');
 
     $.ajax({
       url: bookmarkUrl, 
@@ -16,17 +16,14 @@ $(document).ready(function() {
       data: {_token: csrfToken}, // CSRFトークンの送信
       
       success: function(response) {
-        console.log(response);
+        // console.log(response);
           if (isBookmarked) {
-            // bookmarkLink.attr('href', '/tasks/{{ $task->id }}/bookmarks');
-            // targetBookmark.find('i').removeClass('fas fa-bookmark').addClass('far fa-bookmark');
-            bookmarkLink.html('<a href="/tasks/{{ $task->id }}/bookmarks"><i class="far fa-bookmark"></i></a>');
+            // bookmarkLink.html('<a href="/tasks/{{ $task->id }}/bookmarks"><i class="far fa-bookmark"></i></a>');
+            targetBookmark.find('a').html('<i class="far fa-bookmark"></i>'); 
           // location.reload();
         } else {
-          // targetBookmark.find('a').html('<i class="fas fa-bookmark"></i>');
-          // bookmarkLink.attr('href', '/bookmarks/{{ $task->bookmarkedBy(Auth::user())->firstOrfail()->id }}');
-          // targetBookmark.find('i').removeClass('far fa-bookmark').addClass('fas fa-bookmark');
-          bookmarkLink.html('<a href="/bookmarks/{{ $task->bookmarkedBy(Auth::user())->firstOrfail()->id }}"><i class="fas fa-bookmark"></i></a>');
+          // bookmarkLink.html('<a href="/bookmarks/{{ $task->bookmarkedBy(Auth::user())->firstOrfail()->id }}"><i class="fas fa-bookmark"></i></a>');
+          targetBookmark.find('a').html('<i class="fas fa-bookmark"></i>');
           // location.reload();
         }
         
@@ -42,28 +39,23 @@ $(document).ready(function() {
 
   // ページ遷移をせずにタスク削除
   $('.delete-button').click(function(event) {
-    event.preventDefault(); // デフォルトのフォーム送信をキャンセル
-
-    let deleteForm = $(this).closest('form'); // クリックされた削除ボタンの親フォーム要素を取得
-    let deleteUrl = deleteForm.attr('action'); // フォームのアクション属性から削除URLを取得
+    event.preventDefault(); 
+    let deleteForm = $(this).closest('form'); 
+    let deleteUrl = deleteForm.attr('action'); 
     let csrfToken = $('meta[name="csrf-token"]').attr('content');
 
-// Ajaxリクエストの送信
 $.ajax({
-  url: deleteUrl, // 削除するURL
+  url: deleteUrl, 
   type: 'POST',
-  data: {_token: csrfToken, _method: 'DELETE'}, // CSRFトークンとメソッドを送信
+  data: {_token: csrfToken, _method: 'DELETE'}, 
   beforeSend: function(xhr) {
-    xhr.setRequestHeader('X-CSRF-TOKEN', csrfToken); // リクエストヘッダーにCSRFトークンを設定
+    xhr.setRequestHeader('X-CSRF-TOKEN', csrfToken); 
   },
   success: function(response) {
-    // console.log(response);
-    // レスポンスが成功した場合の処理
-    deleteForm.closest('.comment').remove(); // コメントの要素を削除
-    location.reload();
+    deleteForm.closest('.task-').remove();
+    // location.reload();
   },
   error: function(xhr, status, error) {
-    // レスポンスがエラーの場合の処理
     console.error(xhr.responseText);
   }
     });
